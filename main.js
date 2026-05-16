5924a68f2b839c93ce86ca53ad0711e91f550cf4
fatma-10 <fatmaammoiri05@gmail.com>
2026-05-17 00:35:38 +0100

    cleanup: remove unrelated frontend files from other project

--- main.js
+++
@@ -1,9 +0,0 @@
-import './style.css';
-import { initMap } from './js/maps.js';
-import { handleSubmit } from './js/form.js';
-
-// Attach form submit handler
-document.getElementById('signupForm').addEventListener('submit', handleSubmit);
-
-// Initialize the map when the page loads
-window.onload = initMap;
